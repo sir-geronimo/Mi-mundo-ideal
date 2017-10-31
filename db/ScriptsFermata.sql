4731c86908a4ec6b54ff820903a0a60042af0710
Lehkikm <lehkikm@gmail.com>
2017-10-31 01:27:32 -0400

    Modificando: Base de datos He hecho los siguientes cambios:
    - Hacer un nuevo Diagrama Entidad Relación.
    - Rehacer la base de datos en sql.
    
    Falta por extrapolar la base de datos a php. Me dijo Jossy que hay que
    acerle unas adaptaciones antes de hacer la extrapolación. Jossy se va a
    encargar de hacer uso de la base de datos en el php.

--- db/ScriptsFermata.sql
+++ db/ScriptsFermata.sql
@@ -1,30 +1,29 @@
 CREATE DATABASE Fermata;
 USE Fermata;
 
-/* Creacion de las tablas */
-CREATE TABLE Mundos (
-	ID INT NOT NULL AUTO_INCREMENT,
-    Nombre VARCHAR(20) NOT NULL,
-    Descripcion TEXT NOT NULL,
-    PRIMARY KEY(ID)
+CREATE TABLE Jugadores (
+    id int not null,
+        primary key (id_jugador),
 );
-CREATE TABLE Habitantes (
-	ID INT NOT NULL AUTO_INCREMENT,
-	Cantidad INT NULL,
-    ID_Mundo INT,
-    PRIMARY KEY(ID),
-    FOREIGN KEY(ID_Mundo) REFERENCES Mundos(ID)
-);
+
 CREATE TABLE Deseos (
-	ID INT NOT NULL AUTO_INCREMENT,
-    Nombre VARCHAR(20) NOT NULL,
-	Tipo tinyint NOT NULL,
-    Descripcion TEXT NOT NULL,
-    PRIMARY KEY(ID)
-);
-CREATE TABLE Habitantes_Deseos (
-	ID_Habitantes INT NOT NULL,
-	ID_Deseos INT NOT NULL,
-    FOREIGN KEY(ID_Habitantes) REFERENCES Habitantes(ID),
-	FOREIGN KEY(ID_Deseos) REFERENCES Deseos(ID)
+    id_jugador int not null,
+        foreign key(id_jugador) references Jugadores(id),
+    id_mundo int not null,
+        foreign key (id_mundo) references Mundo(id),
+    id int not null,
+        primary key (id),
+    nombre varchar (60) not null,
+    descripcion text not null,
+    tipo tinyint not null,
+        check (tipo <= 2)
+)
+
+CREATE TABLE Mundo (
+    id int not null,
+        primary key(id),
+	porcentaje_distopia int not null,
+    porcentaje_utopia int not null,
+    tipo tinyint not null,
+        check (tipo <= 2)
 );
